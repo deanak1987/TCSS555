93057dc4184d6923d9d9ad9c71fd8f915818d8d1
DeanAK1987 <deanak@uw.edu>
2022-10-28 15:12:02 -0700

    working on branch gains

--- Assignment2/hw2/hw2Practice.py
+++ Assignment2/hw2/hw2Practice.py
@@ -43,15 +43,8 @@ def funTree():
     myTree.children['strong'] = DecisionNode('no')
     return myTree
 
-def entropy(lists):
-    counts = lists.value_counts()
-    counts = counts.to_frame(name='count').reset_index()
-    counts = counts.pivot_table(index=str(target), columns=counts.columns[0], values='count').fillna(0)
-    n = counts.iloc[0,0]
-    y = counts.iloc[1,0]
+def entropy(y, n):
     s = n + y
-    nlog = 0
-    ylog = 0
     if y == 0:
         ylog = 0
     else:
@@ -59,43 +52,98 @@ def entropy(lists):
     if n == 0:
         nlog = 0
     else:
-        nlog = n / s * math.log2(n / s)
-    ent = - (ylog + nlog)
+         nlog = n / s * math.log2(n / s)
+    ent = -(ylog + nlog)
     return ent
 
-def gain_calc(lists, target, ent_prior, s):
-    counts = lists.value_counts()
-    counts = counts.to_frame(name='count').reset_index()
-    counts = counts.pivot_table(index=str(target), columns=counts.columns[0], values='count').fillna(0)
-    ent = 0
-    for column in counts:
-        n = counts[column].values[0]
-        y = counts[column].values[1]
-        # s = n + y
-        nlog = 0
-        ylog = 0
-        if y == 0:
-            ylog = 0
-        else:
-            ylog = y / s * math.log2(y / s)
-        if n == 0:
-            nlog = 0
-        else:
-            nlog = n / s * math.log2(n / s)
-        ent = ent - (ylog + nlog)
-    gain = ent_prior - ent
+
+
+def gain_calc(group, target, ent_prior, s_tot, flag):
+    if flag == 0:
+        counts = group.value_counts()
+        counts = counts.to_frame(name='count').reset_index()
+        counts = counts.pivot_table(index=str(target), columns=counts.columns[0], values='count').fillna(0)
+        ent = 0
+        ent_branch = 0
+        for column in counts:
+            n = counts[column].values[0]
+            y = counts[column].values[1]
+            s = y + n
+            ent_branch = s / s_tot * entropy(y, n)
+            ent = ent + ent_branch
+
+        gain = ent_prior - ent
+    else:
+        # DEVICE WAY FOR GAIN CALCULATION FOR BRANCH ITEMS
+        gain = 1
     return gain
+
+def MyTree(examples,attributes,  branch, target):
+    my_tree = DecisionNode(str(branch))
+    branch_bool = examples.groupby([str(branch)])[str(target)].value_counts().to_frame(name='count').reset_index()
+    branch_bool = branch_bool.pivot_table(index=str(target), columns=branch_bool.columns[0], values='count').fillna(0)
+    df_ent = pd.DataFrame()
+    # calculate branch entropy
+    i = 0
+    new_branch = []
+    for column in branch_bool:
+        n = branch_bool[column].values[0]
+        y = branch_bool[column].values[1]
+        ent = entropy(y,n)
+        if ent == 0:
+            leaf = ''
+            if y > n:
+                leaf = 'yes'
+            else:
+                leaf = 'no'
+            my_tree.children[str(column)] = DecisionNode(leaf)
+            branch_bool = branch_bool.drop(str(column), axis=1)
+        else:
+            df_ent[str(column)] = [ent]
+            i += 1
+    group1 = pd.DataFrame()
+    for e in df_ent:
+        gain = 0
+        for name in attributes:
+            branch_group = examples.groupby([str(branch), str(name)])[str(target)].value_counts().to_frame(name='count').reset_index()
+
+            group1 = branch_group[branch_group[str(branch)] == e]
+            group1 = group1.groupby([str(target)])['count'].sum()
+            y = group1.values[0]
+            s_tot = group1.sum(axis = 0)
+            ent_prior = df_ent.iloc[0][str(e)]
+            temp = gain_calc(group1, target, ent_prior, s_tot, 1)
+            if temp > gain:
+                gain = temp
+    # DEVICE WAY FOR GAIN CALCULATION FOR BRANCH ITEMS
+    x
+    return gain
+
+# Begin IDS algorithm
 def id3(examples, target, attributes):
-    target_data = examples[str(target)].value_counts().to_dict()
-    sample_ent = entropy(examples[str(target)])
-    maxent = 0
-    target_val = []
-    # for ind in columns in
-    list_branch = examples.groupby([str(attributes[3])])[str(target)]
-    print(attributes[3])
-    temp = gain_calc(list_branch, target, sample_ent, examples.shape[0])
-    tree = temp
-    return temp
+    col_names = attributes
+    target_data = examples[str(target)].value_counts()
+    target_data = target_data.to_frame(name='count').reset_index()
+    target_data = target_data.pivot_table(index=str(target), columns=target_data.columns[0], values='count').fillna(0)
+    n = target_data.iloc[0,0]
+    y = target_data.iloc[1,0]
+    # Calculate sample entropy
+    sample_ent = entropy(y,n)
+
+    # Determine the highest information gain (IG)
+    best_attribute = ''
+    high_gain = 0
+    for name in col_names:
+        list_branch = examples.groupby([str(name)])[str(target)]
+        gain = gain_calc(list_branch, target, sample_ent, examples.shape[0], 0)
+        if gain > high_gain:
+            best_attribute = name
+            high_gain = gain
+    # col_names = col_names.remove(str(best_attribute))
+    col_names.remove(best_attribute)
+    # Establish root of tree with the highest IG
+    tree = MyTree(examples, col_names, best_attribute, target)
+    return tree
 
 
 ####################   MAIN PROGRAM ######################
@@ -112,7 +160,7 @@ tree = id3(train,target,attributes)
 # tree.display()
 print(tree)
 
-# Evaluating the tree on the test data
+# # Evaluating the tree on the test data
 # correct = 0
 # for i in range(0,len(test)):
 #     if str(tree.predicts(test.loc[i])) == str(test.loc[i,target]):
